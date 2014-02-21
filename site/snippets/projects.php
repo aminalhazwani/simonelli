daf533cafdfd5d7eb27614fb4e3eff0d58c60a9c
aminalhazwani <amin.alhazwani@gmail.com>
2014-02-21 14:32:39 +0100

    Updated link project url

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -3,13 +3,17 @@
 <ul>
   <?php foreach($data->children()->visible() as $project): ?>
   <li>
-    <figure>
-     <img src="<?php echo $project->images()->first()->url() ?>" alt="<?php echo html($project->title()) ?>" />
-    </figure>
-    <p><?php echo html($project->title()) ?></p>
-    <p><?php echo html($project->year()) ?></p>
+  	<a href="<?php echo $project->url() ?>">
+	    <figure>
+	    	<img src="<?php echo $project->images()->first()->url() ?>" alt="<?php echo html($project->title()) ?>" />
+		    <figcaption>
+		     	<p><?php echo html($project->title()) ?></p>
+		    	<p><?php echo html($project->year()) ?></p>
+		    </figcaption>
+	    </figure>
+	</a>
   </li>
   <?php endforeach ?>
- </ul>
+</ul>
 
 </section>
