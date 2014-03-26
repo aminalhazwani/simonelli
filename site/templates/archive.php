d77143fe3e3a8df0b1bf9af44b5383b47b8b7f28
aminalhazwani <amin.alhazwani@gmail.com>
2014-03-26 18:37:11 +0100

    Styled archive template

--- site/templates/archive.php
+++ site/templates/archive.php
@@ -1,21 +1,28 @@
 <?php snippet('header') ?>
-<?php snippet('menu') ?>
 
-<b>Titolo e testi</b>
-<p><?php echo $page->title() ?></p>
+	<h1 class="title"><?php echo $page->title() ?></h1>
 
-<b>Selected Online Press</b>
-
-
-<b>Last three news</b>
-<?php foreach($page->children()->visible()->flip()->limit(5) as $article): ?>
-  
 	<article>
-		<p><?php echo $article->date('l j F Y') ?></p>
-		<h1><?php echo html($article->title()) ?></h1>
-		<p><?php echo excerpt($article->text(), 500) ?></p>
+		<?php foreach($page->children()->visible()->flip()->limit(5) as $article): ?>
+		<ul>
+			<li>
+				<p><?php echo $article->date('l j F Y') ?></p>
+				<h2><?php echo html($article->title()) ?></h2>
+				<p><?php echo excerpt($article->text(), 500) ?></p>
+				<hr>
+			</li>
+		</ul>
+		<?php endforeach ?>
 	</article>
 
-<?php endforeach ?>
+	<aside>
+		<section>
+			<h3>Selected Online Press</h3>
+		</section>
+
+		<section>
+			<h3>Selected Print Press</h3>
+		</section>
+	</aside>
 
 <?php snippet('footer') ?>
