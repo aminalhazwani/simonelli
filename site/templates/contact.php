31ffe230ea1c47571ab0edcbe6ba3cd604056bb5
aminalhazwani <amin.alhazwani@gmail.com>
2014-03-26 18:42:55 +0100

    Added contact.php

---
+++ site/templates/contact.php
@@ -0,0 +1,25 @@
+<?php snippet('header') ?>
+	
+	<aside>
+		<section>
+			<h3>Contacts</h3>
+			<a class="e-mail" href="mailto:<?php echo $page->mail() ?>"><?php echo $page->mail() ?></a>
+			<p><?php echo $page->phone() ?></p>
+			<hr class="half">
+			<p><?php echo $page->street() ?></p>
+			<p><?php echo $page->city() ?></p>
+			<p><?php echo $page->state() ?></p>
+		</section>
+
+		<section>
+			<h3>Social</h3>
+			<ul>
+				<li><a href="">Facebook</a></li>
+				<li><a href="">Twitter</a></li>
+				<li><a href="">Instagram</a></li>
+				<li><a href="">Linkedin</a></li>
+			</ul>
+		</section>
+	</aside>
+
+<?php snippet('footer') ?>
