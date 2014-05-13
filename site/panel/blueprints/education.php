871ba05ec04bb742a2a441ae1eaf2add6846827d
aminalhazwani <amin.alhazwani@gmail.com>
2014-05-13 23:09:21 +0200

    Added education blueprint

---
+++ site/panel/blueprints/education.php
@@ -0,0 +1,56 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# project blueprint
+
+title: Page
+pages: true
+files: true
+fields:
+  title: 
+    label: Title
+    type:  text
+    required: true
+  date:
+    label: Date
+    type: date
+    format: dd.mm.yy
+    required: true
+  branch:
+    label: Branch
+    type: radio
+    options:
+      work: Work
+      educational: Educational
+    required: true
+  category:
+    required: true
+  	label: Category
+  	type: text
+  orientation:
+    label: Info
+    type: radio
+    default: top
+    options:
+      top: Top
+      bottom: Bottom
+  tags: 
+    label: Tags
+    type:  tags
+    index: all
+  manufacter:
+    required: true
+  	label: Manufacter
+  	type: text
+  manufacterurl:
+    label: Manufacter Url
+    type: text
+  photo:
+  	label: Photographer
+  	type: text
+  materials:
+  	label: Materials
+  	type: tags
+  text: 
+    label: Text
+    type:  textarea
+    size:  large
