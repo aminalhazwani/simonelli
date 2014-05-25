69228b47b5b7d5751c9fa41ec5f67fd41a4aa097
aminalhazwani <amin.alhazwani@gmail.com>
2014-05-25 16:04:16 +0200

    Added mini-footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -2,8 +2,14 @@
         <div class="footer-container">
     	    <footer class="wrapper">
     	        <?php echo kirbytext($site->copyright()) ?>
-    	        <span>Back to top</span>
+    	        <span class="footer-arrow">&#8963;</span><span>Back to top</span>
     	    </footer>
+            <div class="footer-mini">
+                <div class="wrapper">
+                    <p>© 2014 All Rights Reserved.</p>
+                    <span><a href="<?php echo url() ?>news/impressum">Imprint</a> | <a href="http://aminalhazwani.github.io/">Made with &#9829;</a></span>
+                </div>
+            </div>
     	</div>
     </div>
 
